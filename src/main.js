27326c6ba6c0c624e29d1c796fcfa7cd9fafc0fa
vhcaldas <vivianhcaldas@gmail.com>
2019-09-13 17:04:44 -0300

    Mudanças no arquivo Javascript.

---
+++ src/main.js
@@ -0,0 +1,7 @@
+
+document.getElementById("enviar").addEventListener("click", localizarTransporte);
+
+function localizarTransporte (cTransp, cAno) {
+  console.log(cTransp);
+  console.log(cAno);
+}
