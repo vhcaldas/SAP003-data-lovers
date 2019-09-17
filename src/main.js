22918602db2327761aceea73e89e91956d184b7d
vhcaldas <vivianhcaldas@gmail.com>
2019-09-17 16:45:20 -0300

    Changes on main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 const data = INJURIES;
 
-
+let recebeDados = (event) => {
+  event.preventDefault();
+  const chooseTransp = document.getElementById("cTransp").value;
+  const year = document.getElementById("cAno").value;
+  document.getElementById("Resultado").innerHTML = window.data.transportCategory(chooseTransp, year, array);
+}
 
 
 /*fetch('/data/injuries/injuries.json')
@@ -10,21 +15,13 @@ const data = INJURIES;
  console.log(data);
 })
 
-
-let recebeDados = (event) => {
-  event.preventDefault();
-  const chooseTransp = document.getElementById("cTransp").value;
-  const year = document.getElementById("cAno").value;
-  document.getElementById("Resultado").innerHTML = window.data.transportCategory(chooseTransp, year, array);
-}
-
 document.getElementById("enviar").addEventListener("click", recebeDados);
 
 function localizarTransporte (cTransp, cAno) {
   console.log(cTransp);
   console.log(cAno);
 }*/
- o map para pegar os objetos , mapeia para pegar obj por obj se o year que foi selecionado for = ao year 
+ o map para pegar os objetos , mapeia para pegar obj por obj se o year que foi selecionado for = ao year
 
 /*
 const enviaFiltro = app.filterData(data, menufiltro.value);
