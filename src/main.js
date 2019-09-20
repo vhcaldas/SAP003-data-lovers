72ec8b17f4f90cd11dac7230c1b3ff2d85a86f20
Fabiana Fonseca <fsf.fabiana8@gmail.com>
2019-09-20 16:23:06 -0300

    testando o filtro

--- src/main.js
+++ src/main.js
@@ -3,29 +3,70 @@ const data = INJURIES;
 let recebeDados = (event) => {
   event.preventDefault();
   const chooseTransp = document.getElementById("cTransp").value;
-  const year = document.getElementById("cAno").value;
-  document.getElementById("Resultado").innerHTML = window.data.transportCategory(chooseTransp, year, array);
-}
+  const Year = document.getElementById("cAno").value;
+//console.log(chooseTransp);
+
+  for (let i of data){
+      if (i.Year === Year){
+     //    return getElementById("msgresultado").innerHTML += `<p>O numero de acidentes e de ${i[chooseTransp]}</p>`;
+         
+      }
+    //   console.log(i[data].Year)
+  }
+ /* selectFiltro = document.getElementById("caixas-de-selecao");
+  selectFiltro.addEventListener("change", filterData);
+  
+    function filterData() {
+     const filtrado = PRODUCTS.filter (item =>
+     item.product.price.value >= parseFloat 
+     (selectFiltro.value));
+     showTransporte(filtrado);*/
+  }
+  
+      //selectFiltro.addEventListener("change", () => showTransporte(app.filterData(PRODUCTS, parseInt (selectFiltro.value))))
+
+// document.getElementById("resultado").innerHTML = chooseTransp +", "+ Year;
 
 
-/*fetch('/data/injuries/injuries.json')
+document.getElementById("enviar").addEventListener("click", recebeDados);
+
+
+/*function showTransporte(arrayItems){
+    let productDiv = document.getElementById("products");
+    productDiv.innerHTML =
+    ${arrayItems.map(("products") =>)}
+}*/
+
+       
+
+
+/*const searchForTransport = (chooseTransp, Year, array) => {
+
+} 
+
+    for (let i of array) {
+
+    if(data.Year === Year){
+ }
+    
+ }
+  
+
+
+  
+ //document.getElementById("resultado").innerHTML = window.data.transportCategory(chooseTransp, year, array);
+           
+fetch('/data/injuries/injuries.json')
  .then(function(response){
    return response.json()
  }).then(function(data){
  console.log(data);
 })
-
-document.getElementById("enviar").addEventListener("click", recebeDados);
-
 function localizarTransporte (cTransp, cAno) {
   console.log(cTransp);
   console.log(cAno);
-}*/
- o map para pegar os objetos , mapeia para pegar obj por obj se o year que foi selecionado for = ao year
-
-/*
+}
+// o map para pegar os objetos , mapeia para pegar obj por obj se o year que foi selecionado for = ao year
 const enviaFiltro = app.filterData(data, menufiltro.value);
 
-menufiltro.addEventListener("change", () => selecionados(enviaFiltro));
-*/
-
+menufiltro.addEventListener("change", () => selecionados(enviaFiltro));*/
---
+++ src/test.js
@@ -0,0 +1,59 @@
+const ACIDENTES = [
+{ 
+  Total_Injured_Persons: 2351372,
+  Total_Injured_Persons_Air: 263,
+  Total_Injured_Persons_Bus_Occupants: 14000,
+  Total_Injured_Persons_Commuter_Carrier: 0,
+  Total_Injured_Persons_Employee_Or_Worker: 1680,
+  Total_Injured_Persons_Freight_Vessel: 225,
+  Total_Injured_Persons_Gas_Pipeline: 93,
+  Total_Injured_Persons_General_Aviation: 234,
+  Total_Injured_Persons_Hazardous_Liquid_Pipeline: 0,
+  Total_Injured_Persons_Highway: 2332000,
+  Total_Injured_Persons_Highway_Rail_Grade_Crossing: 195,
+  Total_Injured_Persons_Highway_Rail_Grade_Crossing_Railroads: 872,
+  Total_Injured_Persons_Industrial_Or_Other: 144,
+  Total_Injured_Persons_Motorcyclists: 92000,
+  Total_Injured_Persons_On_Demand_Air_Taxi: 15,
+  Total_Injured_Persons_Other_Counts: null,
+  Total_Injured_Persons_Other_Incident: 10000,
+  Total_Injured_Persons_Other_Incident_Transit: 5264,
+  Total_Injured_Persons_Other_Incidents: 4345,
+  Total_Injured_Persons_Other_Incidents_Railroads: 7351,
+  Total_Injured_Persons_Passenger_Car_Occupants: 1292000,
+  Total_Injured_Persons_Passenger_Or_Occupant: 17101,
+  Total_Injured_Persons_Passenger_Vessel: 337,
+  Total_Injured_Persons_Pedalcyclists: 50000,
+  Total_Injured_Persons_Pedestrians: 65000,
+  Total_Injured_Persons_Pipeline: 93,
+  Total_Injured_Persons_Rail: 4689,
+  Total_Injured_Persons_Rail_Freight: 4102,
+  Total_Injured_Persons_Rail_Freight_Highway_Rail_Grade_Crossing: 679,
+  Total_Injured_Persons_Rail_Freight_Other_Incidents: 3009,
+  Total_Injured_Persons_Rail_Freight_Train_Accidents: 51,
+  Total_Injured_Persons_Rail_Freight_Trespassers: 363,
+  Total_Injured_Persons_Rail_Road_Trespassers: 424,
+  Total_Injured_Persons_Railroad: 667,
+  Total_Injured_Persons_Railroad_Alone: 8786,
+  Total_Injured_Persons_Railroad_Train_Accidents: 88,
+  Total_Injured_Persons_Recreational_Boating: 2678,
+  Total_Injured_Persons_Train_Accidents_Rail_Roads: 139,
+  Total_Injured_Persons_Transit_Non_Rail: 16532,
+  Total_Injured_Persons_Transit_Rail: 7513,
+  Total_Injured_Persons_Transit_Total: 24045,
+  Total_Injured_Persons_Trespassers: 61,
+  Total_Injured_Persons_Truck_Occupants_Large: 27000,
+  Total_Injured_Persons_Truck_Occupants_Light: 782000,
+  Total_Injured_Persons_US_Air_Carrier: 14,
+  Total_Injured_Persons_Water: 3384,
+  Total_Injured_Persons_Water_Not_Related_To_Vessel_Casualties: 608,
+  Total_Injured_Persons_Water_Vessel_Related: 98,
+  Year: "2014-01-04"
+  }
+];
+
+
+function pegaSelect(id,value)
+{
+  
+}
