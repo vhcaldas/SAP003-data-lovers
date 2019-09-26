c5052a8a10feea90dfc88aff7c1d523fd76d552b
vhcaldas <vivianhcaldas@gmail.com>
2019-09-26 17:50:35 -0300

    Elaboração do main.js e data.js para a 2ª história de usuário

--- src/data.js
+++ src/data.js
@@ -1,14 +1,52 @@
 const array = INJURIES;
 
 const searchTheCategory = (chooseTransp, Year) => {
-  //  let arrResult = [];
   for (let i of array) {
     if (i.Year === Year) {
-      let resultado = (i[chooseTransp]);
-      return resultado;
+      return i[chooseTransp];
     }
   }
 };
+
+const finding = (selectedYear) => {
+  const findedYear = array.find(obj => obj.Year === selectedYear);
+  return [
+    {
+      Total_Injured_Persons_Passenger_Car_Occupants: findedYear["Total_Injured_Persons_Passenger_Car_Occupants"],
+      Total_Injured_Persons_Bus_Occupants: findedYear["Total_Injured_Persons_Bus_Occupants"],
+      Total_Injured_Persons_Truck_Occupants_Large:findedYear["Total_Injured_Persons_Truck_Occupants_Large"],
+      Total_Injured_Persons_Train_Accidents_Rail_Roads:findedYear["Total_Injured_Persons_Train_Accidents_Rail_Roads"],
+      Total_Injured_Persons_Recreational_Boating: findedYear["Total_Injured_Persons_Recreational_Boating"],
+      Total_Injured_Persons_Air:findedYear["Total_Injured_Persons_Air"],
+      Total_Injured_Persons_Pedestrians:findedYear["Total_Injured_Persons_Pedestrians"],
+      Total_Injured_Persons_Pedalcyclists: findedYear["Total_Injured_Persons_Pedalcyclists"],
+      Total_Injured_Persons_Motorcyclists: findedYear["Total_Injured_Persons_Motorcyclists"]
+    }
+  ]
+}
+
+const sorting = (increasing, decreasing, receivedYeartoFind, selectedYear) => {
+  // console.log(increasing, decreasing, receivedYeartoFind, selectedYear);
+  if (selectedYear && increasing){
+     receivedYeartoFind.sort(function (a,b){
+       //o problema está aqui para eu resolver amanhã =/
+      if (a < b) {
+        return 1
+      } if (a > b) {
+        return -1
+      }
+    });
+
+  }
+
+
+}
+
 window.data = {
   searchTheCategory: searchTheCategory,
+  finding: finding,
+  sorting: sorting
 };
+
+/*filtrei com o fin
+criar outro array de objetos*/
--- src/main.js
+++ src/main.js
@@ -1,11 +1,40 @@
-let receiveData = (event) => {
+const receiveData = (event) => {
   event.preventDefault();
   const chooseTransp = document.getElementById("cTransp").value;
   const Year = document.getElementById("cYear").value;
-  let tela = window.searchTheCategory(chooseTransp, Year);
-  document.getElementById("lista").innerHTML = tela;
+  const result = window.data.searchTheCategory(chooseTransp, Year);
+  document.getElementById("result-1").innerHTML = result;
 
 };
 
+const receiveYeartoFind = (event) => {
+  event.preventDefault();
+  const selectedYear = document.getElementById("year-option").value;
+  const sentYear = window.data.finding(selectedYear);
+  return sentYear;
+  /*const layout = `
+   '<p>Total bla: ${result["Total_Injured_Persons_Passenger_Car_Occupants"]}</p>
+    <p>Total bla: ${result["Total_Injured_Persons_Bus_Occupants"]}</p>
+    <p>Total bla: ${result["Total_Injured_Persons_Truck_Occupants_Large"]}</p>
+    <p>Total bla: ${result["Total_Injured_Persons_Train_Accidents_Rail_Roads"]}</p>
+    <p>Total bla: ${result["Total_Injured_Persons_Recreational_Boating"]}</p>
+    <p>Total bla: ${result["Total_Injured_Persons_Air"]}</p>
+  `
+  document.getElementById("result-2").innerHTML = layout;*/
+};
+
+const receiveOrderToSort = (event) => {
+  event.preventDefault();
+  const increasing = document.getElementById("increasing").value;
+  const decreasing = document.getElementById("decreasing").value;
+  const selectedYear = document.getElementById("year-option").value;
+  const receiveYeartoFind = window.data.finding(selectedYear);
+  const sentSelectionOrder = window.data.sorting(increasing, decreasing, receiveYeartoFind, selectedYear);
+  return sentSelectionOrder;
+}
+
 document.getElementById("enviar").addEventListener("click", receiveData);
 
+document.getElementById("year-option").addEventListener("change", receiveYeartoFind);
+
+document.getElementById("ordering").addEventListener("change", receiveOrderToSort);
